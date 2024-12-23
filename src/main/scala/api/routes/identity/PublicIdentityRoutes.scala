efb69c26920e098632e52edef77d48e84c4957e2
Michael Hunt <mhunt429@gmail.com>
2024-12-23 07:51:23 -0500

    return access token as a cookie value on login requests

--- src/main/scala/api/routes/identity/PublicIdentityRoutes.scala
+++ src/main/scala/api/routes/identity/PublicIdentityRoutes.scala
@@ -1,20 +1,18 @@
 package api.routes.identity
 
 import application.services.identity.IdentityService
-import cats.data.NonEmptyList
 import cats.effect.IO
 import core.identity.LoginRequest
-import core.json.BaseJson.*
 import org.http4s.circe.CirceEntityEncoder.*
 import org.http4s.circe.CirceSensitiveDataEntityDecoder.circeEntityDecoder
 import org.http4s.dsl.Http4sDsl
-import org.http4s.headers.`WWW-Authenticate`
 import org.http4s.server.Router
-import org.http4s.{Challenge, Headers, HttpRoutes}
+import org.http4s.{HttpRoutes, ResponseCookie}
 import utils.Protocol
 
 final case class PublicIdentityRoutes(identityService: IdentityService)
-    extends Http4sDsl[IO] with Protocol {
+    extends Http4sDsl[IO]
+    with Protocol {
 
   private[routes] val prefixPath = "/identity"
 
@@ -22,7 +20,17 @@ final case class PublicIdentityRoutes(identityService: IdentityService)
     case req @ POST -> Root / "login" =>
       req.decode[LoginRequest] { loginRequest =>
         identityService.logUserIn(loginRequest).flatMap {
-          case Some(userWithToken) => Ok(userWithToken)
+          case Some(userWithToken) => {
+            val jwtToken = userWithToken.tokenData.accessToken
+            val cookie = ResponseCookie(
+              name = "x_token",
+              content = jwtToken,
+              path = Some("/"),
+              httpOnly = true,
+              secure = true
+            )
+            Ok(userWithToken).map(_.addCookie(cookie))
+          }
           case _ =>
             BadRequest(
             )
